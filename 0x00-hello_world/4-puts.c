6ef7cc8fcdede0095dde50200acde41083d08eb2
LeoMaina <leonardhophni@gmail.com>
2022-04-07 15:41:07 -0700

    chnages

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -14,10 +14,11 @@
  *
  *      */
 
+
 int main(void)
 {
-	char Mystring[]= "\"Programming is like building a multilingual puzzle";
-	puts(Mystring);
+
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return 0;
 }
