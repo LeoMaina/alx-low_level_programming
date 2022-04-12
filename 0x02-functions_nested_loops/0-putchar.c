4dbcf8e95a58119ebd6200bd48422c0107593f3b
LeoMaina <leonardhophni@gmail.com>
2022-04-12 11:38:24 -0700

    Oh

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char g[i] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(g[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
