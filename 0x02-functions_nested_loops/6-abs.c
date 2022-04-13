fbeba6d692e964918b677d80e49d8d1f2317f3ca
LeoMaina <leonardhophni@gmail.com>
2022-04-13 05:52:39 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - outputs the absolute value of a number
+ *
+ * @int: - integer under check
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
