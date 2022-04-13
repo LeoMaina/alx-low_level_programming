b8da5fe37272eb4dea1fb1d362515ecedb8f703b
LeoMaina <leonardhophni@gmail.com>
2022-04-13 04:50:35 -0700

    Alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for letters both lowercase and uppercase
+ *
+ * @c: character under check
+ *
+ * Return: 1 if letter , 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
