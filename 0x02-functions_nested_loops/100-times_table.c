5048d6395d44c174622a62c2243561f5703d93e1
LeoMaina <leonardhophni@gmail.com>
2022-04-13 15:00:44 -0700

    yaaay

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,9 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
+	if (n >= 0 && n <= 15)
+	{
+
 	for (a = 0; a <= n; a++)
 	{
 		_putchar(48);
@@ -27,7 +30,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(c + '0');
 			}
-			else if (c <= 99)
+			else if (c >9 && c <= 99)
 			{
 				_putchar(' ');
 				_putchar(c / 10 + '0');
@@ -41,6 +44,7 @@ void print_times_table(int n)
 			}
 		}
 		_putchar('\n');
+	}
 	}
 }
 
