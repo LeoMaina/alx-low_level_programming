447af7bd3a5b497b201b9247ede7abff13d2ce44
LeoMaina <leonardhophni@gmail.com>
2022-04-14 16:24:35 -0700

    i

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - do this
+ * @size: parameter
+ */
+
+void print_triangle(int size)
+{
+	int height;
+	int width;
+	int draw;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= (size - height); width++)
+		{
+			_putchar(' ');
+		}
+		for (draw = 1; draw <= height; draw++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
