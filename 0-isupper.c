f05ab066ec6586c41acd9666a5846e7c0a74bbf6
LeoMaina <leonardhophni@gmail.com>
2022-04-14 12:51:35 -0700

    hot

---
+++ 0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letter
+ * @c: paramter
+ * return: int
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
