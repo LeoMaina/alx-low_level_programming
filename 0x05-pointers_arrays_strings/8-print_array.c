489452a20fa78a089855468fb0753ae129f4af22
LeoMaina <leonardhophni@gmail.com>
2022-04-21 05:50:58 -0700

    Initial comit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array
+ *
+ * @a: pointer to array
+ * @n: elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+		printf("\n");
+}
