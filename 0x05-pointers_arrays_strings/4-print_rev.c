06820b439827a21b97fb3c74d4d83a0b0642a258
LeoMaina <leonardhophni@gmail.com>
2022-04-21 04:34:33 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: pointer to string str
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i = 0;
+	int count;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	length = i;
+
+	for (count = length - 1; count > 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
