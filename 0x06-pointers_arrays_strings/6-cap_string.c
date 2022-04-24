75122408107f7a93db77bc33c4ab26a7d02e79e4
LeoMaina <leonardhophni@gmail.com>
2022-04-24 08:43:50 -0700

    Initial commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize after special characters
+ *
+ * @s: character
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char spe[13] = {' ', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}', '\t', '\n'};
+
+	if (s[i] >= 97 && s[i] <= 122)
+	{
+		s[i] = s[i] - 32;
+	}
+	i++;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			for (j = 0; j < 13; j++)
+			{
+				if (s[i - 1] == spe[j])
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
+
+
+
