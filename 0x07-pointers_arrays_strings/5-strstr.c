7f860feb88f931b776af05f78b5eae4b45b36f04
LeoMaina <leonardhophni@gmail.com>
2022-04-26 15:14:31 -0700

    Initial commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strstr - finds occurence of the substring
+ *
+ * @needle: substring
+ * @haystack: string to be scanned
+ * Return: pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j, x;
+
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			x = i;
+			j = 0;
+
+			while (needle[j] != '\0')
+			{
+				if (haystack[x] == needle[j])
+				{
+					x++;
+					j++;
+				}
+				else
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
+
