a6b0a3bf4e1e6376835404ee22437bf54c507e38
LeoMaina <leonardhophni@gmail.com>
2022-04-28 15:37:17 -0700

    Inital commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints length of string
+ *
+ * @s: string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
+
