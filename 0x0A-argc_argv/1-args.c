4256756923c744ea8dec5c6d7eb4dc30d287265f
LeoMaina <leonardhophni@gmail.com>
2022-05-03 13:57:18 -0700

    Initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: pointer to array of string
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
