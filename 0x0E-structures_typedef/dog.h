4e70d7590e4a06021927aa0ad354034361521eba
LeoMaina <leonardhophni@gmail.com>
2022-05-12 12:02:00 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Description
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
