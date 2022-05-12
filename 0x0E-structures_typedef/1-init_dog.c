54adb71e0032add3f33f92dd5865104b30814bad
LeoMaina <leonardhophni@gmail.com>
2022-05-12 12:13:29 -0700

    Intial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize avariable of type struct dog
+ *
+ * @d: first member
+ * @name: second member
+ * @age: third memebr
+ * @owner: fourth member
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
