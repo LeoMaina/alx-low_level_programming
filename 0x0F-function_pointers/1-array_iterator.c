0226c74a64834b39192839ec90430e0627bf1aed
LeoMaina <leonardhophni@gmail.com>
2022-05-16 12:15:04 -0700

    initial commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * array_iterator - function that executes a function given as a 
+ * array_iterator - function that executes a function given as a
  * parameter on each element of an array.
  *
  * @array: array of elements
