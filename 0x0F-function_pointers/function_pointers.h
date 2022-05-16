2533be28f6f658985dca875314808d9f3c8dc168
LeoMaina <leonardhophni@gmail.com>
2022-05-16 12:07:08 -0700

    Initial commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
+#include <stddef.h> /*lib for size_t*/
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
